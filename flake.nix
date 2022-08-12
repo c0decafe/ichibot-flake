56a9abc33202ea50590a9f1d984b3495264febbc
c0decafe <220505+c0decafe@users.noreply.github.com>
2022-08-12 13:47:54 +0300

    refactor

--- flake.nix
+++ flake.nix
@@ -1,47 +1,44 @@
 {
   description = "ichibot-client-app";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
+  nixConfig.bash-prompt = "ichibot-dev> ";
   inputs.src.url = gitlab:Ichimikichiki/ichibot-client-app;
   inputs.src.flake = false;
 
   outputs = {
     self,
       nixpkgs,
-      flake-utils,
       src
-  }: flake-utils.lib.eachDefaultSystem (system:
+  }:
     let
       name = "ichibot";
-      pkgs = nixpkgs.legacyPackages.${system};
-
-      defaultPackage = pkgs.mkYarnPackage {
-        inherit src name;
-        yarnFlags = [ "--offline" "--frozen-lockfile" "--ignore-engines" ];
-        postConfigure = ''
+      supportedSystems = nixpkgs.lib.systems.flakeExposed;
+    in {
+      packages = nixpkgs.lib.genAttrs supportedSystems (system: {
+        ichibot = nixpkgs.legacyPackages.${system}.mkYarnPackage {
+          inherit src name;
+          yarnFlags = [ "--offline" "--frozen-lockfile" "--ignore-engines" ];
+          postConfigure = ''
           cd deps/${name}
           yarn tsc
           sed -i '1i#!/usr/bin/env node' dist/index.js
           chmod +x dist/index.js
           cd ../..
         '';
-      };
-
-      overlay = final: prev: {
-        ichibot = defaultPackage;
-      };
-
-      devShell =
-        pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs-14_x
-            pkgs.yarn
-            pkgs.yarn2nix
-          ];
         };
-    in {
-      inherit defaultPackage overlay devShell;
-    }
-  );
+      });
+
+      devShell = nixpkgs.lib.genAttrs supportedSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs
+              pkgs.yarn
+            ];
+          }
+      );
+
+      overlay = _: super: self.packages.${super.stdenv.hostPlatform.system} or { };
+    };
 }
